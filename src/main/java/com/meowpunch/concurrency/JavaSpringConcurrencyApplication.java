c13894b6aaf1f8f2d488cd7cb6afd839004c2e4b
jinhoon.bae <jinhoon.bae@adevinta.com>
2022-11-06 16:54:57 +0100

    listenableFuture with callback for non-blocking asynchronous

--- src/main/java/com/meowpunch/concurrency/JavaSpringConcurrencyApplication.java
+++ src/main/java/com/meowpunch/concurrency/JavaSpringConcurrencyApplication.java
@@ -2,7 +2,6 @@ package com.meowpunch.concurrency;
 
 import java.util.concurrent.Future;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -11,6 +10,7 @@ import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.AsyncResult;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.stereotype.Component;
+import org.springframework.util.concurrent.ListenableFuture;
 
 @Slf4j
 @EnableAsync
@@ -25,8 +25,15 @@ public class JavaSpringConcurrencyApplication {
   static class MyService {
 
     @Async
-    public Future<String> hello() throws InterruptedException {
-      log.info("myService.hello() processing... ");
+    public java.util.concurrent.Future<String> futureHello() throws InterruptedException {
+      log.info("myService.futureHello() processing... ");
+      Thread.sleep(2000);
+      return new AsyncResult<>("Hello");
+    }
+
+    @Async
+    public org.springframework.util.concurrent.ListenableFuture<String> listenableFutureHello() throws InterruptedException {
+      log.info("myService.listenableFutureHello() processing... ");
       Thread.sleep(2000);
       return new AsyncResult<>("Hello");
     }
@@ -39,15 +46,28 @@ public class JavaSpringConcurrencyApplication {
   }
 
   @Bean
-  ApplicationRunner run() {
+  ApplicationRunner runFuture() {
     return args -> {
-      log.info("run Application");
+      log.info("run future");
 
-      Future<String> future = myService.hello();
+      Future<String> futureHello = myService.futureHello();
 
       Thread.sleep(1000);
-      log.info("Is it done processing myService.hello()? " + future.isDone());
-      log.info("exit with " + future.get());
+      log.info("Is it done processing " + futureHello.getClass().getName() + "? " + futureHello.isDone());
+      log.info("exit with " + futureHello.get()); // asynchronous and blocking
+    };
+  }
+
+  @Bean
+  ApplicationRunner runListenableFuture() {
+    return args -> {
+      log.info("run listenableFuture");
+
+      ListenableFuture<String> listenableFutureHello = myService.listenableFutureHello();
+      listenableFutureHello.addCallback(str -> log.info("exit with " + str), err -> log.error(err.getMessage()));
+
+      Thread.sleep(1000);
+      log.info("Is it done processing " + listenableFutureHello.getClass().getName() + "? " + listenableFutureHello.isDone()); // unnecessary
     };
   }
 }
